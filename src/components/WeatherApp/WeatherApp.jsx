a36841f34d311036a3efbd14a64a2407a09804fc
macaepifani <macarenaepifani@hotmail.com>
2024-01-05 04:44:51 -0300

    weatherapp

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -31,14 +31,34 @@ const WeatherApp = () => {
         const location = document.getElementsByClassName("weather-location")
         
         humidity[0].innerHTML = data.main.humidity+"%"
-        wind[0].innerHTML = data.wind.speed+"km/h"
-        temp[0].innerHTML = data.main.temp+"°C"
+        wind[0].innerHTML =Math.floor(data.wind.speed)+"km/h"
+        temp[0].innerHTML = Math.floor(data.main.temp)+"°C"
         location[0].innerHTML = data.name
 
         if(data.weather[0].icon==="01d" || data.weather[0].icon ==="01n"){
             setWicon(clear_icon)
         }
-        else if(data.weather[0].icon==="01d" || data.weather[0].icon ==="01n")
+        else if(data.weather[0].icon==="02d" || data.weather[0].icon ==="02n"){
+            setWicon(cloud_icon)
+        }
+        else if(data.weather[0].icon==="03d" || data.weather[0].icon ==="03n"){
+            setWicon(drizzle_icon)
+        }
+        else if(data.weather[0].icon==="04d" || data.weather[0].icon ==="04n"){
+            setWicon(drizzle_icon)
+        }
+        else if(data.weather[0].icon==="09d" || data.weather[0].icon ==="09n"){
+            setWicon(rain_icon)
+        }
+        else if(data.weather[0].icon==="10d" || data.weather[0].icon ==="10n"){
+            setWicon(rain_icon)
+        }
+        else if(data.weather[0].icon==="13d" || data.weather[0].icon ==="13n"){
+            setWicon(snow_icon)
+        }
+        else{
+            setWicon(clear_icon)
+        }
 
     }
 
